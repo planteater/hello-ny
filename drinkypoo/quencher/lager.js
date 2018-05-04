09a15f98a29c1bcb21ee6f53e4b90127264a917e
David Hersh <dave@codestream.com>
2018-05-04 11:59:27 -0400

    larry example

---
+++ drinkypoo/quencher/lager.js
@@ -0,0 +1,5 @@
+const Blamer = require('./util/blamer');
+const BlameViewController = require('./controllers/blameViewController');
+const errorController = require('./controllers/errorController');
+const Directory = require('pathwatcher').Directory
+const path = require('path');
