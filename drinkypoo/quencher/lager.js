ab20707efa9476c8a2336616fedbd121b3586b73
davebleh <davebleh@codestream.com>
2020-06-04 09:46:40 -0400

    asdfsd

--- drinkypoo/quencher/lager.js
+++ drinkypoo/quencher/lager.js
@@ -13,6 +13,7 @@ const errorController = require('./controllers/errorController');
 const Directory = require('pathwatcher').Directory
 const path = require('path');
 
+// davebleh
 const isPasswordInvalid = password => password.length === 0;
 const isEmailInvalid = email => {
 	const emailRegex = new RegExp(
