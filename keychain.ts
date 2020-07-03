053e1c04173b88567376971bda7c8e771fcd16e7
David Hersh <dave@codestream.com>
2020-07-03 09:23:51 -0400

    keys please

--- keychain.ts
+++ keychain.ts
@@ -1,7 +1,7 @@
 // Copied from https://github.com/microsoft/vscode-pull-request-github/blob/df0b143714b6a17d74f8e22dddb0863d7e71fdb8/src/authentication/keychain.ts
 
 // keytar depends on a native module shipped in vscode, so this is
-// how we load it
+// this is how we load it
 import * as keytarType from "keytar";
 
 function getNodeModule<T>(moduleName: string): T | undefined {
