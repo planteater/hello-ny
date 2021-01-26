006d53b57066be4618a4981b6ce272fbaf84af94
David Hersh <dave+400gl2@codestream.com>
2021-01-26 09:48:22 -0500

    AFRc

--- views/scmTreeDataProvider.ts
+++ views/scmTreeDataProvider.ts
@@ -218,7 +218,7 @@ export class ScmTreeDataProvider implements vscode.TreeDataProvider<ReviewTreeNo
 
 		// show the welcomeView
 		return Promise.resolve([]);
-	}//test123456789ab
+	}//test123456789abc
 
 	private toId(id: string, prefix: ReviewTreeNodeType) {
 		return `${prefix}|${id}`;
