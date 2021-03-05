fc6921bf8340346ab6d5fdbb5282d631de060a66
David Hersh <dave@codestream.com>
2021-03-05 16:59:27 -0500

    auto fr telem

--- container.ts
+++ container.ts
@@ -18,7 +18,7 @@ import { SelectionDecorationProvider } from "./providers/selectionDecorationProv
 import { SetServerUrlRequestType} from "./protocols/agent/agent.protocol";
 // import { WebviewSidebarActivator } from "./views/webviewSidebarActivator";
 
-// test fun
+// test funss
 export class Container {
 	static async initialization(
 		context: ExtensionContext,
