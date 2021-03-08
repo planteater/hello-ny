46393151bc2fcbda888f2845ca1fd74eee87a324
Dave Smith <dave+222@codestream.com>
2021-03-08 16:02:45 -0500

    Real branch test 2

--- providers/markerCodeLensProvider.ts
+++ providers/markerCodeLensProvider.ts
@@ -23,7 +23,7 @@ import { configuration } from "../configuration";
 import { Container } from "../container";
 import { Logger } from "../logger";
 import { Strings } from "../system";
-// change from dave+222
+// change from dave+222, and now for branch test
 export class CodemarkCodeLensProvider implements CodeLensProvider, Disposable {
 	static selector: DocumentSelector = { scheme: "file" };
 
