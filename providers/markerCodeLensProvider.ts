33dad04f2685d2dcc57cfe71ae7671ff117c2b2a
Dave Smith <dave+222@codestream.com>
2021-03-08 17:39:34 -0500

    Account for mutliple HEAD changes

--- providers/markerCodeLensProvider.ts
+++ providers/markerCodeLensProvider.ts
@@ -24,7 +24,7 @@ import { Container } from "../container";
 import { Logger } from "../logger";
 import { Strings } from "../system";
 // change from dave+222, and now for branch test
-// moar testing take 2, and change some more
+// moar testing take 2, and change some more and more
 export class CodemarkCodeLensProvider implements CodeLensProvider, Disposable {
 	static selector: DocumentSelector = { scheme: "file" };
 
