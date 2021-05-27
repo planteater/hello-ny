d167923490665df0654f44287e27c96413e8805d
David Hersh <dave@codestream.com>
2021-05-27 16:29:13 -0400

    autofr timeout 3

--- controllers/liveShareController.ts
+++ controllers/liveShareController.ts
@@ -119,7 +119,7 @@ export class LiveShareController implements Disposable {
 	private async onLiveShareSessionChanged(e: SessionChangeEvent) {
 		const vslsId = e.session.id;
 		this.setVslsId(vslsId);
-		// If we aren't signed in or in an active (remote) live share session kick out12
+		// If we aren't signed in or in an active (remote) live share session kick out123
 		if (Container.session.status !== SessionStatus.SignedIn || vslsId == null) return;
 
 		// If we are in an active (remote) live share session, open the liveshare channel
