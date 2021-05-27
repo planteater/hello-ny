41eb8f4a3bfd33641f30958165cc71f695b881d5
David Hersh <dave@codestream.com>
2021-05-27 11:25:58 -0400

    commit while disco

--- container.ts
+++ container.ts
@@ -18,7 +18,7 @@ import { SelectionDecorationProvider } from "./providers/selectionDecorationProv
 import { SetServerUrlRequestType} from "./protocols/agent/agent.protocol";
 // import { WebviewSidebarActivator } from "./views/webviewSidebarActivator";
 
-// test fun
+// test funs
 export class Container {
 	static async initialization(
 		context: ExtensionContext,
