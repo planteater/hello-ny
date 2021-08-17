6f2f7c4867d25d9c62efd2bcdfb3ca5a8622218f
David Hersh <dhersh@newrelic.com>
2021-08-17 12:45:40 -0400

    autoc?

--- container.ts
+++ container.ts
@@ -18,7 +18,7 @@ import { SelectionDecorationProvider } from "./providers/selectionDecorationProv
 import { SetServerUrlRequestType} from "./protocols/agent/agent.protocol";
 // import { WebviewSidebarActivator } from "./views/webviewSidebarActivator";
 
-// test funs
+// test funs1
 export class Container {
 	static async initialization(
 		context: ExtensionContext,
